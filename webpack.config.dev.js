77f2e03a4e832d94b2b37250554dd97e4ea94b34
ryntico <Fubn[f,1@>
2024-12-22 02:30:15 +0300

    chore: добавил линтер

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,14 @@
 const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.config.common')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = merge(commonConfig, {
   mode: 'development',
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js'],
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     port: 3000,
