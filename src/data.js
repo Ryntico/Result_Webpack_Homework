3930af9bb153754d1b40694c4d764b3b9435047c
ryntico <Fubn[f,1@>
2024-12-21 23:34:04 +0300

    chore: реализован рендер

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const buttons = [
+  { id: 1,
+    title: 'rain',
+    sound: './assets/sounds/rain.mp3',
+    backgroundImage: './assets/rainy-bg.jpg',
+    icon: './assets/icons/cloud-rain.svg'
+  },
+  { id: 2,
+    title: 'summer',
+    sound: './assets/sounds/summer.mp3',
+    backgroundImage: './assets/summer-bg.jpg',
+    icon: './assets/icons/sun.svg'
+  },
+  { id: 3,
+    title: 'winter',
+    sound: './assets/sounds/winter.mp3',
+    backgroundImage: './assets/winter-bg.jpg',
+    icon: './assets/icons/cloud-snow.svg'
+  },
+]
+
+export const pauseIcon = './assets/icons/pause.svg'
